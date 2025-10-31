93a178a9723033ee0e1c58c233c5729f93b79181
Edd022 <garciajulian258rgmail.com>
2025-10-31 15:56:12 -0500

    feat: clase principal y recursos de la aplicación

---
+++ OpenWeatherAPI/src/main/java/com/acmspring/co/openweatherapi/OpenWeatherApiApplication.java
@@ -0,0 +1,13 @@
+package com.acmspring.co.openweatherapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWeatherApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenWeatherApiApplication.class, args);
+    }
+
+}
