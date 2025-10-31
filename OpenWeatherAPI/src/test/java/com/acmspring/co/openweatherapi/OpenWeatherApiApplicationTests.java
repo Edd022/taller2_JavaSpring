8cd3fcce527c759a3faa53db8cfe70f38d815bba
Edd022 <garciajulian258rgmail.com>
2025-10-31 15:56:41 -0500

    test: estructura inicial de pruebas

---
+++ OpenWeatherAPI/src/test/java/com/acmspring/co/openweatherapi/OpenWeatherApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acmspring.co.openweatherapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenWeatherApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
